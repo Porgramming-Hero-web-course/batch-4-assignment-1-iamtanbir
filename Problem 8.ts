53c5d80d928fbb8c0b3d2397f6ef2619f3bd4b2d
iamtanbir <mdtanbirahmed271116@gmail.com>
2025-03-22 06:26:20 +0600

    problem 8

---
+++ Problem 8.ts
@@ -0,0 +1,16 @@
+function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    
+    if (!(key in obj)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const person = { name: "Alice", age: 25, email: "alice@example.com" };
+
+console.log(validateKeys(person, ["name", "age"])); 
+console.log(validateKeys(person, ["name", "email"]));
